e08fb40d0ef5ae45f54f2547040acf8b34c6d3d3
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 22:07:35 +0200

    Fix

--- src/com/gmail/falistos/KillRewards/EventListener.java
+++ src/com/gmail/falistos/KillRewards/EventListener.java
@@ -35,59 +35,94 @@ public class EventListener implements Listener {
    @SuppressWarnings("deprecation")
 @EventHandler
    public void onEntityDeath(EntityDeathEvent event) {
+	   
       Player killer = event.getEntity().getKiller();
+      
       if(killer != null) {
+    	  
          if(killer instanceof Player) {
+        	 
+        	if (killer.getName() == null) return;
+        	 
             LivingEntity victim = event.getEntity();
             String section = victim.getType().toString();
+            
             if(killer.hasPermission("killrewards.earn")) {
+            	
                if(this.plugin.getConfig().getBoolean("rewards." + section + ".enabled")) {
+            	   
                   if(this.plugin.isFromSpawner(victim.getUniqueId())) {
+                	  
                      this.plugin.spawnerEntities.remove(victim.getUniqueId());
+                     
                      if(!this.plugin.getConfig().getBoolean("spawners.enable")) {
                         return;
                      }
+                     
                   }
 
                   double reward = 0.0D;
                   String victimName = null;
                   String message = null;
+                  
                   if(victim instanceof Player) {
+                	  
                      Player minReward = (Player)victim;
                      victimName = minReward.getDisplayName();
+                     
                      double percentageTransfert = this.plugin.getConfig().getDouble("rewards.PLAYER.percentageTransfert");
                      reward = this.plugin.economy.getBalance(minReward.getName()) * percentageTransfert;
                      reward = Utils.round(reward, 2);
+                     
                      if(reward > this.plugin.getConfig().getDouble("rewards.PLAYER.maximumTransfert")) {
+                    	 
                         reward = this.plugin.getConfig().getDouble("rewards.PLAYER.maximumTransfert");
+                        
                      }
 
                      this.plugin.economy.withdrawPlayer(minReward.getName(), reward);
                      String victimMessage = this.plugin.getConfig().getString("rewards.PLAYER.victimMessage");
+                     
                      if(victimMessage != null && !victimMessage.isEmpty()) {
+                    	 
                         this.plugin.sendMessage(minReward, killer.getDisplayName(), victimName, victimMessage, Double.valueOf(reward));
+                        
                      }
+                     
                   } else {
+                	  
                      victimName = this.plugin.getConfig().getString("rewards." + section + ".name");
+                     
                      if(victim.getCustomName() != null && this.plugin.getConfig().isConfigurationSection("rewards." + ChatColor.stripColor(victim.getCustomName()))) {
+                    	 
                         victimName = victim.getCustomName();
+                        
                         if(EntityType.valueOf(victimName) == null) {
+                        	
                            section = ChatColor.stripColor(victim.getCustomName());
+                           
                         }
+                        
                      }
 
                      double minReward1 = this.plugin.getConfig().getDouble("rewards." + section + ".minReward");
                      double maxReward = this.plugin.getConfig().getDouble("rewards." + section + ".maxReward");
                      double random = (new Random()).nextDouble();
                      reward = minReward1 + random * (maxReward - minReward1);
+                     
                      if(this.plugin.getConfig().getDouble("globalMultiplier") != 0.0D) {
+                    	 
                         reward *= this.plugin.getConfig().getDouble("globalMultiplier");
+                        
                      }
 
                      reward *= this.plugin.getPermissionMultiplier(killer);
+                     
                      if(this.plugin.isFromSpawner(victim.getUniqueId()) && this.plugin.getConfig().getBoolean("spawners.enable")) {
+                    	 
                         reward *= this.plugin.getConfig().getDouble("spawners.multiplier");
                         int droppedExp = (int)((double)event.getDroppedExp() * this.plugin.getConfig().getDouble("spawners.experienceMultiplier"));
+                        
                         if(droppedExp <= 0) {
                            droppedExp = 1;
                         }
@@ -104,7 +139,10 @@ public class EventListener implements Listener {
                      message = this.plugin.getConfig().getString("rewards.DEFAULT.killerMessage");
                   }
 
+                  
+                  
                   this.plugin.economy.depositPlayer(killer.getName(), reward);
+                  
                   if(message != null && !message.isEmpty()) {
                      this.plugin.sendMessage(killer, killer.getDisplayName(), victimName, message, Double.valueOf(reward));
                   }
